1471cd0d6e9851a012aaf9a05e6474b53a485cc3
Faisallm <faisallawan1997@gmail.com>
2023-10-16 21:03:59 -0700

    Corrected some bugs in routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import './App.css';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
-import Artist from './Pages/Artist/Artist';
-import ArtDetail from './Pages/ArtistDetail/ArtDetail';
-import Blog from './Pages/Blog/Blog';
-import BlogDetail from './Pages/BlogDetail/BlogDetail';
-import Events from './Pages/Events/Events';
-import EventDetail from './Pages/EventDetail/EventDetail';
-import Fashion from './Pages/FashionDesigners/Fashion';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Artist from "./Pages/Artist/Artist";
+import ArtDetail from "./Pages/ArtistDetail/ArtDetail";
+import Blog from "./Pages/Blog/Blog";
+import BlogDetail from "./Pages/BlogDetail/BlogDetail";
+import Events from "./Pages/Events/Events";
+import EventDetail from "./Pages/EventDetail/EventDetail";
+import Fashion from "./Pages/FashionDesigners/Fashion";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route to='/'  element={Home}/>
-        <Route to='/about'  element={About}/>
-        <Route to='/artist'  element={Artist}/>
-        <Route to='/artist-detail'  element={ArtDetail}/>
-        <Route to='/blog'  element={Blog}/>
-        <Route to='/blog-detail'  element={BlogDetail}/>
-        <Route to='/event'  element={Events}/> 
-        <Route to='/events-detail'  element={EventDetail}/> 
-        <Route to='/fashion-designers'  element={Fashion}/>  
-      </Routes>
-      <Footer/>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/artist" element={<Artist />} />
+          <Route path="/artist-detail" element={<ArtDetail />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog-detail" element={<BlogDetail />} />
+          <Route path="/event" element={<Events />} />
+          <Route path="/events-detail" element={<EventDetail />} />
+          <Route path="/fashion-designers" element={<Fashion />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import Logo from '../HomePageAssets/logo.png';
-import './header.css';
+import React from "react";
+import Logo from "../HomePageAssets/logo.png";
+import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='header'>
-        <div className="logo">
-            <img src={Logo} alt="Logo" />
-        </div>
-        <ul className="navigation-links">
-            <li>About</li>
-            <li>Creative</li>
-            <li>Events</li>
-            <li>Blog</li>
-        </ul>
-        <div className="get-ticket">
-            Get your ticket
-        </div>
+    <div className="header">
+      <div className="logo">
+        <Link to='/'>
+        <img src={Logo} alt="Logo" />
+        </Link>
+      </div>
+      <ul className="navigation-links">
+        <li>
+          <Link to="/about" style={{ textDecoration: "none", color: "#000" }}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/fashion-designers" style={{ textDecoration: "none", color: "#000" }}>
+            Creative
+          </Link>
+        </li>
+        <li>
+          <Link to="/event" style={{ textDecoration: "none", color: "#000" }}>
+            Events
+          </Link>
+        </li>
+        <Link to="/blog" style={{ textDecoration: "none", color: "#000" }}>
+          Blog
+        </Link>
+      </ul>
+      <div className="get-ticket">Get your ticket</div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
