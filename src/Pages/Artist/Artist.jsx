1b1ed5f2e66cc837c4a2e96d53d689ef4de00146
Faisallm <faisallawan1997@gmail.com>
2023-10-22 22:38:24 -0700

    Done with Artist

--- src/Pages/Artist/Artist.jsx
+++ src/Pages/Artist/Artist.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import "./Artist.css";
+import Artist1 from "../../Components/ArtistAssets/artist1.jpg";
+import Artist2 from "../../Components/ArtistAssets/artist2.jpg";
+import Artist3 from "../../Components/ArtistAssets/artist3.jpg";
+import Artist4 from "../../Components/ArtistAssets/artist4.jpg";
+import Artist5 from "../../Components/ArtistAssets/artist5.jpg";
+import TwoImage from "../../Components/TwoImage/TwoImage";
+import OneImage from "../../Components/OneImage/OneImage";
 
 const Artist = () => {
   return (
-    <div>Artist</div>
-  )
-}
+    <div className="artist">
+      <TwoImage
+        fashion1={Artist1}
+        fashion2={Artist2}
+        name1="IMRAN TILDE"
+        name2="NEW PHASE"
+        name3="ABDULRAZAQ AHMED"
+        name4="KNOWMADS"
+      />
+      <OneImage
+        fashion1={Artist3}
+        name1="ALMOGERA ABDALBAGEA"
+        name2="SARINA WEAR"
+      />
+      <TwoImage
+        fashion1={Artist4}
+        fashion2={Artist5}
+        name1="SALI SAMORA"
+        name2="SARINA WEAR"
+        name3="SALI SAMORA"
+        name4="SARINA WEAR"
+      />
+    </div>
+  );
+};
 
-export default Artist
+export default Artist;
