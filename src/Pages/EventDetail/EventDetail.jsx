fa326b654292b6c0a4b52f24c32ff56b9761a6f0
Faisallm <faisallawan1997@gmail.com>
2023-11-01 18:26:30 -0700

    Working on events details page

--- src/Pages/EventDetail/EventDetail.jsx
+++ src/Pages/EventDetail/EventDetail.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import "./EventDetail.css";
+import Event1 from "../../Components/EventAssets/event1.png";
+import Event2 from "../../Components/EventAssets/event2.png";
+import Event3 from "../../Components/EventAssets/event3.png";
+import Event0 from "../../Components/EventAssets/event0.png";
 
 const EventDetail = () => {
+  const location = useLocation();
+  const { title } = location.state;
   return (
-    <div>EventDetail</div>
-  )
-}
+    <div className="event-detail">
+      <div className="little-title">
+        events <span>/{title}</span>
+      </div>
+      <div className="main-title">{title}</div>
+      <ul className="main-stats">
+        <li>29 april</li>
+        <li>1k+ audiences</li>
+        <li>20 designers</li>
+        <li>10 artists</li>
+        <li>10 exhibitions</li>
+      </ul>
+      <div className="intro-text">
+        Our exhibitions in 2021 were a testament to the diversity and depth of
+        African creativity. From contemporary masterpieces that pushed the
+        boundaries of artistic expression to traditional crafts that celebrated
+        heritage, each showcase was a captivating journey into the heart of
+        African art. We witnessed vibrant colors, intricate details, and
+        thought-provoking narratives that left a lasting impression on all who
+        visited.
+      </div>
+      <div className="gallery">
+        <div className="gallery-title">galleria</div>
+        <div class="scroller">
+          <div class="scroll-container">
+            <div class="scroll-image">
+              <img src={Event1} alt="scroll-image" />
+              <p>oladimeji odonsi</p>
+            </div>
+            <div class="scroll-image">
+              <img src={Event2} alt="scroll-image" />
+              <p>oladimeji odonsi</p>
+            </div>
+            <div class="scroll-image">
+              <img src={Event2} alt="scroll-image" />
+              <p>oladimeji odonsi</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EventDetail
+export default EventDetail;
--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -7,23 +7,30 @@ const Events = () => {
     <div className="events">
       <p className="event-title">Our events</p>
       <div className="events-cards">
-        <div className="event-card">
-          <div className="card-title">#KAFART 2023</div>
-          <div className="card-ticket">Get your Ticket</div>
-        </div>
+        <Link to="/events-detail" state={{ title: "#KAFART 2023" }}>
+          <div className="event-card">
+            <div className="card-title">#KAFART 2023</div>
+            <div className="card-ticket">Get your Ticket</div>
+          </div>
+        </Link>
 
-        <div className="events-cards">
-          <div className="event-card">
-            <div className="card-title">#KAFART 2022</div>
-            <div className="card-ticket">Get your Ticket</div>
+        <Link to="/events-detail" state={{ title: "#KAFART 2022" }}>
+          <div className="events-cards">
+            <div className="event-card">
+              <div className="card-title">#KAFART 2022</div>
+              <div className="card-ticket">Get your Ticket</div>
+            </div>
           </div>
-        </div>
-        <div className="events-cards">
-          <div className="event-card">
-            <div className="card-title">#KAFART 2021</div>
-            <div className="card-ticket">Get your Ticket</div>
+        </Link>
+
+        <Link to="/events-detail" state={{ title: "#KAFART 2021" }}>
+          <div className="events-cards">
+            <div className="event-card">
+              <div className="card-title">#KAFART 2021</div>
+              <div className="card-ticket">Get your Ticket</div>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
