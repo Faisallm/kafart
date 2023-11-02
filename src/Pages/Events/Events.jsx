316da6d78d3a8f8765fe43ee087d03577405e717
Faisallm <faisallawan1997@gmail.com>
2023-11-01 17:05:42 -0700

    done with first events page

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./Events.css";
+import { Link } from "react-router-dom";
 
 const Events = () => {
   return (
-    <div>Events</div>
-  )
-}
+    <div className="events">
+      <p className="event-title">Our events</p>
+      <div className="events-cards">
+        <div className="event-card">
+          <div className="card-title">#KAFART 2023</div>
+          <div className="card-ticket">Get your Ticket</div>
+        </div>
 
-export default Events
+        <div className="events-cards">
+          <div className="event-card">
+            <div className="card-title">#KAFART 2022</div>
+            <div className="card-ticket">Get your Ticket</div>
+          </div>
+        </div>
+        <div className="events-cards">
+          <div className="event-card">
+            <div className="card-title">#KAFART 2021</div>
+            <div className="card-ticket">Get your Ticket</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Events;
